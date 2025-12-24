6a76cd34869a358cdea89f1c423cc4ae26363bf9
luzruz555 <seongirum555@gmail.com>
2025-12-24 23:13:34 +0900

    Update status.js

--- functions/api/status.js
+++ functions/api/status.js
@@ -19,19 +19,19 @@ export async function onRequest(context) {
   const relations = relation.split('.');
 
   let relationLines = '';
-for (let i = 0; i < chars.length; i++) {
-  let x, y;
-  if (i < 5) {
-    x = 552;
-    y = 340 + (i * 48);
-  } else {
-    x = 900;
-    y = 340 + ((i - 5) * 48);
+  for (let i = 0; i < chars.length; i++) {
+    let x, y;
+    if (i < 5) {
+      x = 506;
+      y = 340 + (i * 48);
+    } else {
+      x = 825;
+      y = 340 + ((i - 5) * 48);
+    }
+    const charName = chars[i].length > 6 ? chars[i].substring(0, 6) + '..' : chars[i];
+    const rel = (relations[i] || '???').length > 10 ? (relations[i] || '???').substring(0, 10) + '..' : (relations[i] || '???');
+    relationLines += `<text x="${x}" y="${y}" fill="white" font-size="34" font-family="'Noto Sans KR', sans-serif" font-weight="200">${charName} | ${emojis[i] || '?'} | ${rel}</text>`;
   }
-  const charName = chars[i].length > 6 ? chars[i].substring(0, 6) + '..' : chars[i];
-  const rel = (relations[i] || '???').length > 10 ? (relations[i] || '???').substring(0, 10) + '..' : (relations[i] || '???');
-  relationLines += `<text x="${x}" y="${y}" fill="white" font-size="34" font-family="'Noto Sans KR', sans-serif" font-weight="200">${charName} | ${emojis[i] || '?'} | ${rel}</text>`;
-}
 
   const bgUrl = url.origin + '/status-bg.png';
   const bgResponse = await fetch(bgUrl);
@@ -39,21 +39,21 @@ for (let i = 0; i < chars.length; i++) {
   const bgBase64 = btoa(String.fromCharCode(...new Uint8Array(bgBuffer)));
 
   const svg = `
-    <svg width="1200" height="628" xmlns="http://www.w3.org/2000/svg">
+    <svg width="1100" height="628" xmlns="http://www.w3.org/2000/svg">
       <defs>
         <style>
           @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@200;700&amp;display=swap');
         </style>
       </defs>
-      <image href="data:image/png;base64,${bgBase64}" width="1200" height="628"/>
-      <text x="35" y="177" fill="white" font-size="26" font-family="'Noto Sans KR', sans-serif" font-weight="200">${location}</text>
-      <text x="392" y="177" fill="white" font-size="26" font-family="'Noto Sans KR', sans-serif" font-weight="200">${date}</text>
-      <text x="569" y="177" fill="white" font-size="26" font-family="'Noto Sans KR', sans-serif" font-weight="200">${time}</text>
-      <text x="746" y="177" fill="white" font-size="26" font-family="'Noto Sans KR', sans-serif" font-weight="200">${job}</text>
-      <text x="132" y="383" fill="white" font-size="36" font-family="'Noto Sans KR', sans-serif" font-weight="700" text-anchor="middle">${factionDisplay}</text>
-      <text x="132" y="437" fill="white" font-size="18" font-family="'Noto Sans KR', sans-serif" font-weight="200" text-anchor="middle">${ability}</text>
+      <image href="data:image/png;base64,${bgBase64}" width="1100" height="628"/>
+      <text x="32" y="177" fill="white" font-size="26" font-family="'Noto Sans KR', sans-serif" font-weight="200">${location}</text>
+      <text x="359" y="177" fill="white" font-size="26" font-family="'Noto Sans KR', sans-serif" font-weight="200">${date}</text>
+      <text x="522" y="177" fill="white" font-size="26" font-family="'Noto Sans KR', sans-serif" font-weight="200">${time}</text>
+      <text x="684" y="177" fill="white" font-size="26" font-family="'Noto Sans KR', sans-serif" font-weight="200">${job}</text>
+      <text x="121" y="383" fill="white" font-size="36" font-family="'Noto Sans KR', sans-serif" font-weight="700" text-anchor="middle">${factionDisplay}</text>
+      <text x="121" y="437" fill="white" font-size="18" font-family="'Noto Sans KR', sans-serif" font-weight="200" text-anchor="middle">${ability}</text>
       ${relationLines}
-      <text x="276" y="570" fill="white" font-size="20" font-family="'Noto Sans KR', sans-serif" font-weight="200">${incident}</text>
+      <text x="253" y="570" fill="white" font-size="20" font-family="'Noto Sans KR', sans-serif" font-weight="200">${incident}</text>
     </svg>
   `;
 
